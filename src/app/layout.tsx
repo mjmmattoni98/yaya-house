41d839100875ea70d2cfa51ce0229610290dbdd3
Marcos Javier Magni Mattoni <javier.magni@gmail.com>
2026-07-06 17:30:17 +0200

    Add suppressHydrationWarning to root html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
